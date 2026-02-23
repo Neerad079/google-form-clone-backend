db82d708cddd4a515af7064735bcc2567f2ea900
NEERAD <neerad_2024bcse079@nitsri.ac.in>
2026-02-23 22:37:02 +0530

    First Commit

---
+++ googleformclone/src/main/java/com/neerad/googleformclone/GoogleformcloneApplication.java
@@ -0,0 +1,13 @@
+package com.neerad.googleformclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleformcloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleformcloneApplication.class, args);
+	}
+
+}
---
+++ googleformclone/src/test/java/com/neerad/googleformclone/GoogleformcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neerad.googleformclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleformcloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
